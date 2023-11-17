cace0821141c694ef9a8c185385fa081e9645afb
joannapolec <asia.polec@gmail.com>
2023-11-17 15:01:02 +0100

    answer commited

--- js-quiz/theory-questions/hoisting.js
+++ js-quiz/theory-questions/hoisting.js
@@ -1,6 +1,6 @@
 // Without running the script please mark which one of these console logs will return a string and which one will cause the program to fail. Explain why.
 
-console.log(x()); // this one will return the string because x is defined and y is not
+console.log(x()); // this one will return the string because x is defined in the function and y is not (before conole.log)
 
 console.log(y()); // ANSWER:
 
