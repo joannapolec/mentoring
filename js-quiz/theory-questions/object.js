6b8bb938f0aaca83baae1b0f11399c74963bed16
joannapolec <asia.polec@gmail.com>
2023-11-20 10:14:24 +0100

    commit message

--- js-quiz/theory-questions/object.js
+++ js-quiz/theory-questions/object.js
@@ -8,11 +8,15 @@ const person = {
 // I want to add the `employed` property which should be set to true to this object. How would I do it below? Do not change the code above.
 
 // ANSWER (BELOW):
+person.employed = "true";
 
 // Write the `name` property to the console using `person` variable
 
 // ANSWER (BELOW):
+console.log(person.name)
+
 
 // Use hasOwnProperty() method to determine if the `person` object has a property called `status`
 
 // ANSWER (BELOW):
+console.log(person.hasOwnProperty('status'))
