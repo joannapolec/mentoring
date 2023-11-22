cded506008e846cc94d4a7ccf931f4de13a2d3d9
joannapolec <asia.polec@gmail.com>
2023-11-22 18:18:20 +0100

    updated footer

--- components/wine-footer/wineFooter.js
+++ components/wine-footer/wineFooter.js
@@ -5,7 +5,9 @@ class wineFooter extends HTMLElement {
     super();
 
     this._winefooter = {
-    text: 'Lorem ipsum dolor sit amet 2023.',
+    text: '© Lorem ipsum dolor sit amet 2023.',
+    h3: 'Footer Content',
+    p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     };
   }
 
@@ -17,11 +19,38 @@ class wineFooter extends HTMLElement {
     const footerBody = document.createElement('div');
     footerBody.classList.add('footer-body');
 
-    const p = document.createElement('p');
-    p.classList.add('footer-body-p');
-    p.textContent = this._winefooter.text;
+    const content = document.createElement('div');
+   content.classList.add('footer-content');
 
-    footerBody.appendChild(p);
+   const contentLeft = document.createElement('div');
+   contentLeft.classList.add('footer-content-info');
+
+   const h3 = document.createElement('h3');
+   h3.classList.add('h3');
+   h3.textContent = this._winefooter.h3;
+
+   const p = document.createElement('p');
+   p.classList.add('p');
+   p.textContent = this._winefooter.p; 
+   
+   const contentRight = document.createElement('div');
+   contentRight.classList.add('footer-content-links');
+
+ //  const links = document.createElement('ul');
+   //links.classList.add('');
+
+    const copyright = document.createElement('div');
+    copyright.classList.add('footer-copyright');
+    copyright.textContent = this._winefooter.text;
+    
+    footerBody.appendChild(content);
+    content.appendChild(contentLeft);
+    contentLeft.appendChild(h3);
+    contentLeft.appendChild(p);
+
+    content.appendChild(contentRight);
+
+    footerBody.appendChild(copyright);
 
     shadow.appendChild(footerBody);
   }
