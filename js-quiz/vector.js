7d06e5a73d16f3f122ed407a12b9ecf7b659e240
joannapolec <asia.polec@gmail.com>
2023-11-22 11:07:54 +0100

    weifhh

--- js-quiz/vector.js
+++ js-quiz/vector.js
@@ -1,5 +1,8 @@
 // This function should accept `length` number and return an array of randomly generated number (the array is of length `length`). Use Math.random().
 // For negative value of `length`, return an empty array.
-const vector = () => {};
+const vector = () => {
+
+};
 
 export default vector;
+
